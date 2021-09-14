b74dd9cb14aa691d3b8226554a7c96df165e8556
Ekaterina Fedorova <ek8terinafed@gmail.com>
2021-09-14 10:19:38 -0500

    write collatz_conjecture function to return steps

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
 def steps(number):
-    pass
+    steps = 0
+    while number != 1:
+        if number % 2 == 0:
+            number = number / 2
+        else:
+            number = number * 3 + 1
+        steps = steps + 1
+    return(steps)
+
+print(steps(9))
